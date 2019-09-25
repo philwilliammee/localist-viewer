71a2b6c6f9b7a5f542c054d2bd1181781c1a407f
philwilliammee <philliammee@yahoo.com>
2019-09-25 07:34:24 -0400

    umd module

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 
+// Mostly from https://medium.com/@BrodaNoel/how-to-create-a-react-component-and-publish-it-in-npm-668ad7d363ce
 var path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
