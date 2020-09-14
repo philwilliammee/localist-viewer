fc54bcd33da3376b681921485f40e48d179f1081
philwilliammee <phil.williammee@gmail.com>
2020-09-14 14:48:17 -0400

    added calendar

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {LocalistComponent} from 'react-localist-viewer';
+import {LocalistComponent} from './dist';
 
 const localistRun = props => {
     const {
