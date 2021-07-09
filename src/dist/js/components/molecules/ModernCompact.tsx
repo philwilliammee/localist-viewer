b7ce1842830f7390269b4b1c65512ebacea697a9
Alison Jo <2660348+alisonjo315@users.noreply.github.com>
2021-07-09 16:29:11 -0400

    typo/spelling fix

--- src/dist/js/components/molecules/ModernCompact.tsx
+++ src/dist/js/components/molecules/ModernCompact.tsx
@@ -78,7 +78,7 @@ const ModernCompact = (props: StandardProps) => {
                 );
               })
             ) : (
-              <p>There are no upcomming events.</p>
+              <p>There are no upcoming events.</p>
             )}
           </div>
         </div>
